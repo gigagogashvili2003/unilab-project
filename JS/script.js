c404f4d4b941e7117737d8b020161243207fe66b
gigagogashvili2003 <75528697+gigagogashvili2003@users.noreply.github.com>
2021-02-28 09:38:11 -0800

    added everything such as responsive fonts and etc
    complete everything

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,35 @@
+//   when user clicks on burger-icon
+// jquery
 
 $("#burger-menu").on("click", function(){
     $("#menu").toggleClass("open-menu");
-    $("#socicon").toggleClass("open-socicon");
   })
 
-  document.querySelector('.burger-icon').addEventListener('click',function() {
-      document.querySelector('.header-wraper').classList.toggle('active');
-      document.querySelector('.contact-section1').classList.toggle('active-1');
-      document.querySelector('.contact-section2').classList.toggle('active-1');
-      document.querySelector('.contact-section3').classList.toggle('active-1');
+//   when user clicks on burger-icon
+  $(".burger-icon").on("click", function () {
+      $(".header-wraper").toggleClass("active");
+      $(".contact-section1").toggleClass("active-1");
+      $(".contact-section2").toggleClass("active-1");
+      $(".contact-section3").toggleClass("active-1");
+      $(".section1").toggleClass("active-1");
+      $(".section2").toggleClass("active-1");
+      $(".section3").toggleClass("active-1");
+      $(".avatar-svg").toggleClass("active-1");
   })
+  // native javascript
+
+  // time :)
+
+  let clock = document.querySelector("#clock");
+  let date = document.querySelector("#date")
+
+setInterval(function() {
+  let date = new Date();
+  clock.innerHTML = date.toLocaleTimeString();
+});
+setInterval(function() {
+  let currentDate = new Date();
+  date.innerHTML = currentDate.toLocaleDateString();
+})
+
 
